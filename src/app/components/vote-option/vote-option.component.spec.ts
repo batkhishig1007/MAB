108857fddbc92bac60b4da7300c2b456ca4b3b0b
Munkhdulguun <munkhdulguun.mib@gmail.com>
2020-12-16 20:34:59 +0800

    a

---
+++ src/app/components/vote-option/vote-option.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VoteOptionComponent } from './vote-option.component';
+
+describe('VoteOptionComponent', () => {
+  let component: VoteOptionComponent;
+  let fixture: ComponentFixture<VoteOptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoteOptionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VoteOptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
